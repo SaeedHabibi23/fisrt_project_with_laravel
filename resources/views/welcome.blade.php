7ded9c95a1245a36847db66cf1500c93366c5d3c
SaeedHabibi23 <ghulamsaeed54@gmail.com>
2024-07-14 18:13:40 +0430

    Project Created

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel</title>
+    <link rel="stylesheet" href="{{ asset('style.css')}}">
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+</head>
+<body>
+
+    <a href="aboutUs" class="btn btn-primary"> About Us </a>
+
+    <h1> Hello World </h1>
+
+    
+
+
+    <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+</body>
+</html>
